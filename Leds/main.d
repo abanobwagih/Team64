515f44110a816e7bafea3b3d58472f810b06aacf
Abanob Wagih 1802411 <bebo.w.b@gmail.com>
2021-06-06 12:58:09 +0200

    first commit

---
+++ Leds/main.d
@@ -0,0 +1,10 @@
+.\main.o: main.c
+.\main.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\main.o: C:\Keil_v5\ARM\Inc\TI\TM4C123\TM4C123.h
+.\main.o: C:\Keil_v5\ARM\Inc\TI\TM4C123\TM4C123GH6PM.h
+.\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4.h
+.\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4_simd.h
+.\main.o: C:\Keil_v5\ARM\Inc\TI\TM4C123\system_TM4C123.h
+.\main.o: C:/Keil_v5/EE319Kware/inc/tm4c123gh6pm.h
